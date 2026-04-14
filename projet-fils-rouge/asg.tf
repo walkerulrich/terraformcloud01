bf5fa03a58fab6725fddd3525b891587a35ee769
walkerulrich <167113306+walkerulrich@users.noreply.github.com>
2026-04-14 09:39:10 +0200

    nouveau projet

--- projet-fils-rouge/asg.tf
+++ projet-fils-rouge/asg.tf
@@ -12,7 +12,7 @@ resource "aws_autoscaling_group" "my_asg" {
 # Create Launch Configuration
 resource "aws_launch_configuration" "my_lc" {
     name                 = "my-lc"
-    image_id             = "ami-0d71ca6a78e324f68"  # Replace with your desired AMI ID
+    image_id             = "ami-0c7217cdde317cfec"  # Replace with your desired AMI ID
     instance_type        = "t3.micro"      # Replace with your desired instance type
     security_groups      = [aws_security_group.my_security_group.id]
 }
