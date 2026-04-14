ae6cbfb3ff158f6ec05592b189ec85b42ad79d99
walkerulrich <167113306+walkerulrich@users.noreply.github.com>
2026-04-14 11:54:50 +0200

    fatigué projet final

--- projet-fils-rouge/provider.tf
+++ projet-fils-rouge/provider.tf
@@ -1,18 +1,12 @@
 terraform {
   required_version = "1.14.8"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
   cloud {
+    
     organization = "terraformclouds-org"
 
     workspaces {
-      name = "projet-fils-rouge_ff"
+      name = "projet-fils-rouge_fatigue"
     }
   }
 }
