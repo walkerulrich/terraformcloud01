1af183bfef23e7789a9913ead4603b919fd8565c
walkerulrich <167113306+walkerulrich@users.noreply.github.com>
2026-04-14 11:31:35 +0200

    peut-etre projet final

--- projet-fils-rouge/provider.tf
+++ projet-fils-rouge/provider.tf
@@ -1,26 +1,22 @@
 terraform {
+  required_version = "1.14.8"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-}
-terraform {
-  required_version = "1.14.8"
-
-terraform {
-  required_version = "1.14.8"
 
   cloud {
-    
     organization = "terraformclouds-org"
 
     workspaces {
       name = "projet-fils-rouge_ff"
     }
-   }
+  }
 }
+
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
